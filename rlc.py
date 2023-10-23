374fd9796ca48c4d9ec975533585eec5e4736ff6
John Batarekh <j.batarekh@gmail.com>
2023-10-23 11:28:52 -0400

    Created Photon class and main file, edited RLC class to be more specific towards our goal. Main file needs modification to run more efficiently and have clear meaning.

--- rlc.py
+++ rlc.py
@@ -8,81 +8,40 @@ class RLC():
     This class initializes the ode of series RLC circuit and
     solves it. The ode is in the form of y''+2*alpha*y'+omega^2*y=0.
     """
-
-    def alpha(r, l):
-        """
-        Defines alpha parameter for the ode.
-
-        Inputs:
-        -------
-        r: float, resistance of the system.
-        l: float, inductor of the system.
-
-        Returns:
-        --------
-        alp: float, alpha parameter of the system.
-        """
-        alp = r/2*l
-        return alp
     
-    def omega_0(l, c):
-        """
-        Defines omega parameter for the ode.
-
-        Inputs:
-        -------
-        l: float, inductor of the system.
-        c: float, capacidance of the system.
-
-        Returns:
-        --------
-        omega: float, omega parameter of the system.
-        """
-
-        omega = 1/np.sqrt(l*c)
-        return omega
-    
-    def rlc_ode(t, v, r, l, c):
-        """
-        Defines the ode with vector v = [y', y].
+    def __init__(self, r,l,c, tint, ic):
+        self.alpha = r/(2*l)
+        self.omega = 1/np.sqrt(l*c)
+        self.tint = tint
+        self.ic = ic
         
-        Inputs:
+    def solve_ivp(self, n):
+        '''
+        This function puses the scipy library to compute the 4th order Runge-Kutta method integration to find the posistion of the oscillator as a function of time
+
+        Parameters
+        ----------
+        n : int
+            number of points to analyze inbetween the initial and final time
+
+        Returns
         -------
-        t: list, time span for limits of integration.
-        v: list, vector that defines y' and y.
-        r: float, resistance of the system.
-        l: float, inductor of the system.
-        c: float, capacidance of the system.
+        t : ndarray
+            time series used to calculate x
+        x : ndarray
+            posistion series calculated with t
 
-        Return:
-        -------
-        ode: list, ode in the form of
-        y'' = -2*alpha*y' - 2*omega^2*y        
+        '''
+        step = (self.tint[1]-self.tint[0])/n
 
-        """
-        alp = alpha(r, l)
-        omeg = omega_0(l, c)
-        ode =  [-2*alp*v[1], -2*omeg**2*v[0]]
-        return ode
-    
-    def solve_ivp(r, l, c, v0, t):
-        """
-        Solves the ode with scipy.integrate.solve_ivp.
+        def odes(t, x):
+            dx = np.zeros((2))
+            dx[0] = x[1]
+            dx[1] = -self.alpha*x[1]-self.omega**2*x[0]
 
-        Inputs:
-        -------
-        r: float, resistance of the system.
-        l: float, inductor of the system.
-        c: float, capacidance of the system.
-        v0: list, initial conditions.   
-        t: list, time span for limits of integration.
+            return dx
 
-        Returns:
-        --------
-        y: numpy_array, solutions for y
-        time: numpy array, time steps.
-        """
-        soln = si.solve_ivp(rlc_ode, t, v0, args=(r, l, c))
-        y = soln.y
-        time = soln.t
-        return y, time
+        Xres = si.solve_ivp(
+            odes, self.tint, self.ic, method='RK45', max_step=step)
+
+        return Xres.t, Xres.y[0], Xres.y[1]
