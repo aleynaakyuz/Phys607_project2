7ff09fc661e782e84bd6193ea470f8832aab4fdc
Aleyna Akyuz <aleyna@Aleyna-MBP-5.lan>
2023-10-25 06:56:43 -0400

    Adding some documentation and requirements.txt

--- src/rlc.py
+++ src/rlc.py
@@ -6,29 +6,60 @@ class RLC():
 
     """
     This class initializes the ode of series RLC circuit and
-    solves it. The ode is in the form of y''+2*alpha*y'+omega^2*y=0.
+    related operations. The ode is in the form of y''+2*alpha*y'+omega^2*y=0.
     """
     
     def __init__(self, r,l,c, tint, ic):
-        self.alpha = r/(2*l)
+        self.alpha = r/(2*l) 
         self.omega = 1/np.sqrt(l*c)
-        self.tint = tint
-        self.ic = ic
+        self.tint = tint #time interval
+        self.ic = ic #initial conditions
 
     
     def odes(self, t, x):
+        """
+        This function initilizes the ode of the form 
+        y''+2*alpha*y'+omega^2*y=0.
+
+        Parameters
+        -----------
+        t: list
+            time interval of the ode
+        x: list
+            values for y and y'
+
+        Returns
+        --------
+        dx: ndarray
+            solutions of the ode.
+        """
         dx = np.zeros((2))
         dx[0] = x[1]
         dx[1] = -self.alpha*x[1]-self.omega**2*x[0]
         return dx
     
-    def power(self, r, x):
-        I_rms = np.sqrt(np.mean(x**2))
-        return I_rms**2*r
+    def power(self, r, i):
+        """
+        This function calculates the power of a rlc circut with 
+        the formula power = I_rms^2 * r where I_rms is root-mean-square of
+        current and r is resistance. 
+
+        Parameters
+        -----------
+        r: int
+            resistance
+        i: ndarray
+            current  
+        """
+        I_rms = np.sqrt(np.mean(i**2))
+        power = I_rms**2*r
+        return power
         
     def solve_ivp(self, n):
         '''
-        This function puses the scipy library to compute the 4th order Runge-Kutta method integration to find the posistion of the oscillator as a function of time
+        This function puses the scipy library to compute the 4th order 
+        Runge-Kutta method integration to find the posistion of the oscillator 
+        as a function of time
 
         Parameters
         ----------
